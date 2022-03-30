97d5026dcd30fe8d64508ad8cce025270ec6da16
Rohan Andharai <rohanandharia@gmail.com>
2022-03-30 15:51:17 +0530

    database change to mysql

--- task1/task1/settings.py
+++ task1/task1/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'task1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'user',
+        'USER':'root',
+        'PASSWORD':'Rohan_2781',
+        'PORT':3306,
+        'HOST':'127.0.0.1',
     }
 }
 
